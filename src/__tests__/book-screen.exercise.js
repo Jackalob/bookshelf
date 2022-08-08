d2a75900afec59b4c67991c75f47a95f1b430aa9
Jackalob <nike9712@gmail.com>
2022-08-08 10:26:39 +0800

    330

--- src/__tests__/book-screen.exercise.js
+++ src/__tests__/book-screen.exercise.js
@@ -1,15 +1,76 @@
 // 🐨 here are the things you're going to need for this test:
-// import * as React from 'react'
-// import {render, screen, waitFor} from '@testing-library/react'
-// import {queryCache} from 'react-query'
-// import {buildUser, buildBook} from 'test/generate'
-// import * as auth from 'auth-provider'
-// import {AppProviders} from 'context'
-// import {App} from 'app'
+import * as React from 'react'
+import {App} from 'app'
+import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
+import {AppProviders} from 'context'
+import * as auth from 'auth-provider'
+import {buildBook, buildUser} from 'test/generate'
+import {queryCache} from 'react-query'
 
-// 🐨 after each test, clear the queryCache and auth.logout
+afterEach(() => {
+  queryCache.clear()
+  auth.logout()
+})
 
-test.todo('renders all the book information')
+test('renders all the book information', async () => {
+  const user = buildUser()
+  const book = buildBook()
+
+  window.localStorage.setItem(auth.localStorageKey, 'SOME_FAKE_TOKEN')
+  window.history.pushState({}, 'TEST PAGE', `/book/${book.id}`)
+
+  const originalFetch = window.fetch
+  window.fetch = async (url, config) => {
+    if (url.endsWith('/bootstrap')) {
+      return {
+        ok: true,
+        json: async () => ({
+          user: {...user, token: 'SOME_FAKE_TOKEN'},
+          listItems: [],
+        }),
+      }
+    } else if (url.endsWith(`/books/${book.id}`)) {
+      return {
+        ok: true,
+        json: async () => ({book}),
+      }
+    }
+    return originalFetch(url, config)
+  }
+
+  render(<App />, {wrapper: AppProviders})
+
+  await waitForElementToBeRemoved(() => [
+    ...screen.queryAllByLabelText(/loading/i),
+    ...screen.queryAllByText(/loading/i),
+  ])
+  screen.debug()
+
+  expect(screen.getByRole('heading', {name: book.title})).toBeInTheDocument()
+  expect(screen.getByText(book.author)).toBeInTheDocument()
+  expect(screen.getByText(book.publisher)).toBeInTheDocument()
+  expect(screen.getByText(book.synopsis)).toBeInTheDocument()
+  expect(screen.getByRole('img', {name: /book cover/i})).toHaveAttribute(
+    'src',
+    book.coverImageUrl,
+  )
+  expect(screen.getByRole('button', {name: /add to list/i})).toBeInTheDocument()
+
+  expect(
+    screen.queryByRole('button', {name: /remove from list/i}),
+  ).not.toBeInTheDocument()
+  expect(
+    screen.queryByRole('button', {name: /mark as read/i}),
+  ).not.toBeInTheDocument()
+  expect(
+    screen.queryByRole('button', {name: /mark as unread/i}),
+  ).not.toBeInTheDocument()
+  expect(
+    screen.queryByRole('textarea', {name: /notes/i}),
+  ).not.toBeInTheDocument()
+  expect(screen.queryByRole('radio', {name: /star/i})).not.toBeInTheDocument()
+  expect(screen.queryByLabelText(/start date/i)).not.toBeInTheDocument()
+})
 // 🐨 "authenticate" the client by setting the auth.localStorageKey in localStorage to some string value (can be anything for now)
 
 // 🐨 create a user using `buildUser`
@@ -26,7 +87,7 @@ test.todo('renders all the book information')
 // 💰 return Promise.resolve({ok: true, json: async () => ({ /* response data here */ })})
 
 // 🐨 render the App component and set the wrapper to the AppProviders
-// (that way, all the same providers we have in the app will be available in our tests)
+// (that way, all the same providers we have in the app will be avail able in our tests)
 
 // 🐨 use findBy to wait for the book title to appear
 // 📜 https://testing-library.com/docs/dom-testing-library/api-async#findby-queries
